ac71a9b0474d36803fded0b4f7688f240e5eca7b
Wilbert Chan <l22070034@valladolid.tecnm.mx>
2026-05-25 15:49:28 -0600

    componente8
    se implemento el componente 8

--- components/Avatar.js
+++ components/Avatar.js
@@ -5,8 +5,28 @@ export default class Avatar extends Component {
     //renderizamos el componente
     render() {
         //crearemos nuestro etiquetas 
+        const box = document.createElement("div");
         const image = document.createElement("img");
-        const title = document.createElement("h3");
+        const name = document.createElement("h3");
         const description = document.createElement("p");
+
+        //asignamos los valores que recibimos por props a cada una de las etiquetas
+        image.src = this.props.src;
+        image.width = '100';
+
+        name.innerText = this.props.name;
+        description.innerText = this.props.role;
+
+        //creamos un contenedor para nuestro avatar
+        box.append(
+            image,
+            name,
+            description
+        )
+
+        //devolvimos el contenedor con todo el contenido para que el renderer lo pueda renderizar en el DOM
+        return box;
+
+
     }
 }
